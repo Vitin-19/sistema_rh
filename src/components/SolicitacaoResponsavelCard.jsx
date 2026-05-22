1f1e92ad42980ff5b4f7bee3f999062b218f7a57
PEDRO MARINHO RODRIGUES CAMARGO CORRÊA <pedro.correa45@etec.sp.gov.br>
2026-05-22 11:16:02 -0300

    upadate: solicitacaoResponsavel

--- src/components/SolicitacaoResponsavelCard.jsx
+++ src/components/SolicitacaoResponsavelCard.jsx
@@ -1,21 +1,20 @@
-const SolicitacaoResponsavelCard = ({ professor, disciplina, status, onApprove, onReject, onView }) => {
+const SolicitacaoResponsavelCard = ({ professor,onApprove, onReject, onView }) => {
   return (
     <article className="card-solicitacao">
       <div className="informacoes">
-        <h2>Professor</h2>
-        <p>{professor}</p>
-
-        <h2>Disciplina</h2>
-        <p>{disciplina}</p>
+        <div className="TextoProfessor">
+          <h2>Professor</h2>
+          <p>{professor}</p>
+        </div>
+        <button className="button-view" onClick={onView}>
+          Visualizar
+        </button>
 
       </div>
 
       <div className="card-actions">
         <button className="button-approve" onClick={onApprove}>
           Aprovar
-        </button>
-        <button className="button-view" onClick={onView}>
-          Visualizar
         </button>
         <button className="button-reject" onClick={onReject}>
           Rejeitar
